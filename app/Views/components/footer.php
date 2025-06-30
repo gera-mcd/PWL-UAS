1e33a4f730c4222c032ebabcb3ccc1f2a0b7f472
Yosa-svg <yosafat19226@gmail.com>
2025-06-30 23:03:07 +0800

    Menambahkan footer

---
+++ app/Views/components/footer.php
@@ -0,0 +1,16 @@
+ <!-- Footer -->
+            <footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Zellion</span></strong>. All Rights Reserved
+    </div>
+                  <a
+                    href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
+                    target="_blank"
+                    class="footer-link me-4"
+                    >Support</a
+                  >
+                </div>
+              </div>
+            </footer>
+            <!-- / Footer -->
+        
