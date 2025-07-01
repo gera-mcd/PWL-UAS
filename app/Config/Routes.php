c11483a418a4040f442c97c745dfa616150dcc7a
sultansahrul <sultannqu@gmail.com>
2025-07-01 12:20:13 +0700

    Menambahkan Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,11 +10,15 @@ $routes->get('/', 'Home::index', ['filter' => 'auth']);
 $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
- 
-$routes->get('login','auth-login-basic.html::index');
 
 $routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 
+$routes->get('login','auth-login-basic.html::index');
+
+$routes->get('profile','AccountController::profile', ['filter' => 'auth']);
+$routes->get('account','AccountController::account', ['filter' => 'auth']);
+
 $routes->get('dashboard', 'DashboardController::index');
+   
 ;
