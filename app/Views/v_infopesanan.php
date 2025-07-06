39eef13a4e224d455d3ba9fbf65d241554591249
HilmiIrfani <hilmiirfani04804@gmail.com>
2025-07-06 21:21:48 +0700

    Menambahkan v_infopesanan

---
+++ app/Views/v_infopesanan.php
@@ -0,0 +1,24 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+History Transaksi Pembelian <strong><?= $username ?></strong>
+<hr>
+<div class="table-responsive">
+    <!-- Table with stripped rows -->
+    <table class="table datatable">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">ID Pembelian</th>
+                <th scope="col">Waktu Pembelian</th>
+                <th scope="col">Total Bayar</th>
+                <th scope="col">Alamat</th>
+                <th scope="col">Status</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+    <!-- End Table with stripped rows -->
+</div>
+<?= $this->endSection() ?>
