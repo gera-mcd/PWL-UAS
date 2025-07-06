b5e6fbe28ad5d68a4f72bd61f094e62323f7f994
HilmiIrfani <hilmiirfani04804@gmail.com>
2025-07-06 21:24:14 +0700

    Menambahkan sold_produk

---
+++ app/Views/sold_produk.php
@@ -0,0 +1,21 @@
+<?= $this->extend('layout') ?>
+
+<?= $this->section('content') ?>
+
+<h1>Produk Terjual</h1>
+
+<table class="table table-striped">
+  <thead class="table-thead-dark">
+    <tr>
+      <th>No</th>
+      <th>Nama Produk</th>
+      <th>Harga</th>
+      <th>Jumlah</th>
+      <th>Total</th>
+    </tr>
+  </thead>
+  <tbody>
+
+  </tbody>
+</table>
+<?= $this->endSection() ?>
