b7101d9353f1adade79ec2bd6abb970046067c8d
Yosa-svg <yosafat19226@gmail.com>
2025-07-06 22:06:22 +0800

    Menambahkan form label di checkout

---
+++ app/Views/v_checkout.php
@@ -0,0 +1,30 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+
+<div class="row">
+    <div class="col-lg-6">
+        <?= form_open('buy', 'class="row g-3"') ?>
+        <?= form_hidden('username', session()->get('username')) ?>
+        <?= form_input(['type' => 'hidden', 'name' => 'total_harga', 'id' => 'total_harga', 'value' => '']) ?>
+
+        <div class="col-12">
+            <label for="nama" class="form-label">Nama</label>
+            <input type="text" class="form-control" id="nama" value="<?= session()->get('username') ?>" readonly>
+        </div>
+        <div class="col-12">
+            <label for="alamat" class="form-label">Alamat</label>
+            <input type="text" class="form-control" id="alamat" name="alamat" required>
+        </div> 
+        <div class="col-12">
+            <label for="kelurahan" class="form-label">Kelurahan</label>
+            <select class="form-control" id="kelurahan" name="kelurahan" required></select>
+        </div>
+        <div class="col-12">
+            <label for="layanan" class="form-label">Layanan</label>
+            <select class="form-control" id="layanan" name="layanan" required></select>
+        </div>
+        <div class="col-12">
+            <label for="ongkir" class="form-label">Ongkir</label>
+            <input type="text" class="form-control" id="ongkir" name="ongkir" readonly>
+        </div>
+    </div>
