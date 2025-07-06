d15f25e83c704c5ee5a07b63be60a925c23fac79
Yosa-svg <yosafat19226@gmail.com>
2025-07-06 21:58:19 +0800

    Update v_account

--- app/Views/v_account.php
+++ app/Views/v_account.php
@@ -1,5 +1,4 @@
 <?= $this->extend('layout') ?>
-
 <?= $this->section('content') ?>
 <div class="container-xxl flex-grow-1 container-p-y">
   <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Account Settings /</span> Account</h4>
@@ -17,31 +16,14 @@
         <div class="card-body">
           <div class="d-flex align-items-start align-items-sm-center gap-4">
             <img
-              src="<?= base_url('sneat-1.0.0/assets/img/avatars/1.png') ?>"
+              src="<?= base_url('sneat-1.0.0/assets/img/avatars/8.png') ?>"
               alt="user-avatar"
               class="d-block rounded"
               height="100"
               width="100"
               id="uploadedAvatar"
             />
-            <div class="button-wrapper">
-              <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
-                <span class="d-none d-sm-block">Upload new photo</span>
-                <i class="bx bx-upload d-block d-sm-none"></i>
-                <input
-                  type="file"
-                  id="upload"
-                  class="account-file-input"
-                  hidden
-                  accept="image/png, image/jpeg"
-                />
-              </label>
-              <button type="button" class="btn btn-outline-secondary account-image-reset mb-4">
-                <i class="bx bx-reset d-block d-sm-none"></i>
-                <span class="d-none d-sm-block">Reset</span>
-              </button>
 
-              <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
             </div>
           </div>
         </div>
@@ -105,10 +87,7 @@
                 />
               </div>
             </div>
-            <div class="mt-2">
-              <button type="submit" class="btn btn-primary me-2">Save changes</button>
-              <button type="reset" class="btn btn-outline-secondary">Cancel</button>
-            </div>
+            
           </form>
         </div>
         <!-- /Account -->
