da909fda22e796fb86b3ac73bef8a879d6633177
sultansahrul <sultannqu@gmail.com>
2025-07-07 20:10:42 +0700

    Menambahkan addToCart pada TransaksiController

--- app/Controllers/TransaksiController.php
+++ app/Controllers/TransaksiController.php
@@ -9,7 +9,39 @@ class TransaksiController extends BaseController
 {
     public function index()
     {
-        //
-        return view('v_keranjang');
+        $session = session();
+        $keranjang = $session->get('cart') ?? [];
+        $data['keranjang'] = $keranjang;
+        return view('v_keranjang', $data);
+    }
+
+    public function addToCart($productId)
+    {
+        $productModel = new ProductModel();
+        $product = $productModel->find($productId);
+
+        if (!$product) {
+            return redirect()->back()->with('error', 'Produk tidak ditemukan');
+        }
+
+        $session = session();
+        $cart = $session->get('cart') ?? [];
+
+        if (isset($cart[$productId])) {
+            $cart[$productId]['jumlah'] += 1;
+        } else {
+            $cart[$productId] = [
+                'id' => $product['id'],
+                'nama' => $product['nama'],
+                'harga' => $product['harga'],
+                'foto' => $product['foto'],
+                'jumlah' => 1,
+                'weight' => $product['weight'] ?? 0,
+            ];
+        }
+
+        $session->set('cart', $cart);
+
+        return redirect()->to('/keranjang')->with('success', 'Produk berhasil ditambahkan ke keranjang');
     }
 }
