a70706bb272385d848c40a2dd09d20c18ba7a09b
sultansahrul <sultannqu@gmail.com>
2025-07-07 20:31:55 +0700

    Update Routes pada group produk

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -26,8 +26,8 @@ $routes->group('produk', ['filter' => 'auth'], function ($routes) {
     $routes->post('', 'ProdukController::create');
     $routes->post('edit/(:any)', 'ProdukController::edit/$1');
     $routes->get('delete/(:any)', 'ProdukController::delete/$1');
-
-    
+    $routes->get('sold', 'ProdukController::soldProduk');
+    $routes->get('download', 'ProdukController::download');
 });
 
 $routes->get('home', 'ProdukController::category', ['filter' => 'auth']);
