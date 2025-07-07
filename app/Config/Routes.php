611d3727952b52475412284e5213b3da6d67d684
sultansahrul <sultannqu@gmail.com>
2025-07-07 20:40:08 +0700

    Menambahkan Routes buy dan update Routes get-location dan get-cost

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -40,6 +40,9 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
 $routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
 $routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
 $routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+$routes->get('get-location', 'Checkout::getLocation');
+$routes->get('get-cost', 'Checkout::getCost');
 
 $routes->get('home', 'ProdukController::category', ['filter' => 'auth']);
 $routes->get('produk/category', 'ProdukController::category', ['filter' => 'auth']);
