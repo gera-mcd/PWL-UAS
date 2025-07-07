d8e8f265fa2bb28f07861dfe974e499f3f023ff8
sultansahrul <sultannqu@gmail.com>
2025-07-07 19:45:54 +0700

    Menambahkan class tabel pada v_checkout

--- app/Views/v_checkout.php
+++ app/Views/v_checkout.php
@@ -28,3 +28,49 @@
             <input type="text" class="form-control" id="ongkir" name="ongkir" readonly>
         </div>
     </div>
+    <div class="col-lg-6">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nama</th>
+                    <th>Harga</th>
+                    <th>Jumlah</th>
+                    <th>Sub Total</th>
+                    <th>Bobot</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $beratTotal = 0; ?>
+                <?php foreach ($items as $item): ?>
+                    <tr>
+                        <td><?= esc($item['name']) ?></td>
+                        <td><?= number_to_currency($item['price'], 'IDR') ?></td>
+                        <td><?= esc($item['qty']) ?></td>
+                        <td><?= number_to_currency($item['price'] * $item['qty'], 'IDR') ?></td>
+                        <td><?= esc($item['weight'] * $item['qty']) ?> gr</td>
+                    </tr>
+                <?php endforeach; ?>
+                <tr>
+                    <td colspan="2"></td>
+                    <td>Subtotal</td>
+                    <td><?= number_to_currency($total, 'IDR') ?></td>
+                    <td><?= $totalWeight ?> gr</td>
+                </tr>
+                <tr>
+                    <td colspan="2"></td>
+                    <td>Total</td>
+                    <td><span id="total"><?= number_to_currency($total, 'IDR') ?></span></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary">Buat Pesanan</button>
+        </div>
+        <?= form_close() ?>
+    </div>
+</div>
+
+<?= $this->endSection() ?>
+<?= $this->section('script') ?>
