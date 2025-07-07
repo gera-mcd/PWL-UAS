57b0eefe59e54d3e9100083a5ecae2349582e830
Yosa-svg <yosafat19226@gmail.com>
2025-07-07 23:20:48 +0800

    Update v_account

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,7 +16,6 @@ $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 
 $routes->get('login','auth-login-basic.html::index');
 
-$routes->get('profile','AccountController::profile', ['filter' => 'auth']);
 $routes->get('account','AccountController::account', ['filter' => 'auth']);
 
 $routes->get('dashboard', 'DashboardController::index');
--- app/Views/v_profile.php
+++
@@ -1,6 +0,0 @@
-<?= $this->extend('layout') ?>
-
-<?= $this->section('content') ?>
-<h2>Welcome, <?= esc($username) ?>!</h2>
-<p>Your role is: <?= esc($role) ?></p>
-<?= $this->endSection() ?>
